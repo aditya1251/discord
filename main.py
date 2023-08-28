348d6c7a57b64ddaae9da76d6af4eb3c70e3cc7c
aditya1251 <143312844+aditya1251@users.noreply.github.com>
2023-08-28 11:59:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ data = db[cli]
 
 url = "https://discord.com/api/v9/channels/1144174264128389181/messages"
 
-def main2():
-    while True:
-        dat = data.find_one({'cli':cli})
-        if dat:
-            for aut in dat['auths']:
-                try:
-                    auth = {'authorization':aut}
-                    msg = {'content':'!daily'}
-                    requests.post(url,headers=auth,data=msg)
-                    time.sleep(10)
-                except Exception:
-                    continue
-        time.sleep(86400)
+# def main2():
+#     while True:
+#         dat = data.find_one({'cli':cli})
+#         if dat:
+#             for aut in dat['auths']:
+#                 try:
+#                     auth = {'authorization':aut}
+#                     msg = {'content':'!daily'}
+#                     requests.post(url,headers=auth,data=msg)
+#                     time.sleep(10)
+#                 except Exception:
+#                     continue
+#         time.sleep(86400)
 
-time_thread = threading.Thread(target=main2)
-time_thread.start()
+# time_thread = threading.Thread(target=main2)
+# time_thread.start()
 
 while True:
     dat = data.find_one({'cli':cli})
